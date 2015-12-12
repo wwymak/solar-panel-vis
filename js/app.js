5dac75055ce0eee1dd0940f6ccd3a4956a0ff4eb
WingYee Mak <wwymak@gmail.com>
2015-12-12 20:07:19 +0000

    starting to structure the app js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
 /**
- * Created by wwymak on 12/12/2015.
+ * Stuff for actually controlling the vis-- depending on how the code grows may have to split itup
  */
+
+var solarVis = solarVis ||{}
+
+//for data loading, exposing whatever's in exports
+solarVis.dataHandler = function module(){
+    var exports = {},
+        dataDispatch = d3.dispatch() //todo data ready events
+
+    /**
+     *
+     * @param filepath string 'path-to-data-file'
+     * @param dataParseFunc function to process each column e.g. cast to number etc
+     */
+    exports.getData = function(filepath, dataParseFunc){
+
+    }
+}
+
+//d3.csv("data/energy_generation.csv", function(err, data){
+//
+//})
+
+// for the stacked area chart
+solarVis.stackedChart = function module(){
+
+}
+
+//for the map
+//ToDO !!!!!! (google map/ mapbox map with geojson overlay...)
+
