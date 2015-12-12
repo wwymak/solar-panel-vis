8492dc837705846a13810fe03967b20066cf8dd9
WingYee Mak <wwymak@gmail.com>
2015-12-12 20:44:19 +0000

    wip for charting

--- js/app_testground.js
+++ js/app_testground.js
@@ -2,6 +2,7 @@
  * Created by wwymak on 12/12/2015.
  */
 
+var nestedData
 d3.csv("data/energy_generation.csv", function(err, data){
     if(err){
         console.log(err);
@@ -15,5 +16,14 @@ d3.csv("data/energy_generation.csv", function(err, data){
         d.date = timeFormating.parse(d.date);
         d.meter_reading = +d.meter_reading;
         d.output = +d.output;
+        d.installed_capacity_kw = +d.installed_capacity_kw
+        d.postCodeArea = d.postcode.split(" ")[0] //first part of the postcode to group areas by
     })
+    console.log(data)
+
+    nestedData = d3.nest().key(function(d){return d.postCodeArea}).entries(data)
 })
+
+var stackedChartSVG = d3.select("#lineChartContainer").append("svg")
+                        .attr("width", 600)
+                        .attr("height", 500)
