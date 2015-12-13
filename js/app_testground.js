02813c4bdfa770dc768fda92fca57c5381c94062
WingYee Mak <wwymak@gmail.com>
2015-12-13 09:29:09 +0000

    trying out the stack func and area for plotting the stacked chart

--- js/app_testground.js
+++ js/app_testground.js
@@ -2,7 +2,27 @@
  * Created by wwymak on 12/12/2015.
  */
 
-var nestedData
+var nestedData,
+
+    width = 600, height = 500
+
+//scales
+var xTimeScale = d3.time.scale().range([0, width]),
+    yScale = d3.scale.linear().range([height, 0])
+
+//layout funcs
+var stackFunc = d3.layout.stack()
+    .offset("zero")
+    .values(function (d) { return d.values; })
+    .x(function (d) { return xTimeScale(d.key); })
+    .y(function (d) { return d.values; });
+
+var areaFunc = d3.svg.area()
+    .x(function(d) { return xTimeScale(d.date); })
+    .y0(function(d) { return yScale(d.y0); })
+    .y1(function(d) { return yScale(d.y0 + d.y); });
+
+
 d3.csv("data/energy_generation.csv", function(err, data){
     if(err){
         console.log(err);
@@ -21,9 +41,49 @@ d3.csv("data/energy_generation.csv", function(err, data){
     })
     console.log(data)
 
-    nestedData = d3.nest().key(function(d){return d.postCodeArea}).entries(data)
+    //parsing the data
+    nestedData = d3.nest().key(function(d){return d.postCodeArea}).sortKeys(d3.ascending)
+        .key(function(d){return d.date})
+        .rollup(function(leaves){return d3.sum(leaves, function (d){return d.output});})
+        .entries(data);
+
+    /**
+     * basically pass in above nest of nest
+     * @param nestedData
+     */
+    function getMaxDate(nestedData){
+        var maxDateArr = nestedData.map(function(d){
+            return d3.max(d.values, function(k){
+                return (new Date(k.key).getTime())
+            })
+        })
+
+        console.log(maxDateArr)
+        return new Date(d3.max(maxDateArr));
+    }
+    //TODO probably put this in with getMaxDate since using pretty much the same logic
+    function getMinDate(nestedData){
+        var minDateArr = nestedData.map(function(d){
+            return d3.min(d.values, function(k){
+                return (new Date(k.key).getTime())
+            })
+        })
+        return new Date(d3.min(minDateArr));
+    }
+
+    xTimeScale.domain([getMinDate(nestedData), getMaxDate(nestedData)]);
+
 })
+
 
 var stackedChartSVG = d3.select("#lineChartContainer").append("svg")
                         .attr("width", 600)
-                        .attr("height", 500)
+                        .attr("height", 500);
+
+
+
+//axis
+
+//areas
+
+//transitions
