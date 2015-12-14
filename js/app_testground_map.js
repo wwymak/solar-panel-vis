efe768d87d4579271fedd4d544dfe15dc4fb9b73
Wendy <wendy@zoocha.com>
2015-12-14 14:58:39 +0000

    adding in the map zoom to bounding box events-- a lot of stuff to test with mapbox js...

--- js/app_testground_map.js
+++ js/app_testground_map.js
@@ -9,7 +9,29 @@ var map = L.mapbox.map('mapContainer', 'mapbox.emerald').setView([51.5, -2.58], 
 d3.json("data/bristol_postcode.json", function(err, topoData){
     var features = topojson.feature(topoData, topoData.objects.bristol_layers_joined)
     //and add to the map
-    L.geoJson(features).addTo(map);
+    var geoJsonLayer = L.geoJson(features).addTo(map);
+
+    //make the map zoom to the postcodes
+    map.fitBounds(geoJsonLayer.getBounds()); //todo  check if this works
+
+    geoJsonLayer.eachLayer(function (layer) {
+        //adding the id to each postcode polygon so they can be hidden etc to interact with the chart
+        if(typeof layer._path != 'undefined'){
+            layer._path.id = layer.feature.properties.post_area;
+        } else{
+            layer.eachLayer(function (layer2){
+                layer2._path.id = layer.feature.properties.post_area;
+            });
+        }
+
+        //putting this here temporarily -- basically need the click event to also broadcast to the stacked chart via d3.dispatch
+        layer.on('click', function(e){
+            console.log("click", e, e.layer.getBounds())
+            var polygonBounds = e.layer.getBounds()
+            map.fitBounds(polygonBounds)
+        })
+        // this is how you get the bounding box of each postcode area polygon: console.log(layer.getBounds())
+    });
 
     //TODO add some styling!
 })
